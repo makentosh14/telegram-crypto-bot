569dd3db8a3bccef05e06b4e1aae5264f7acd54d
makentosh14 <rimgaudas.veliulis@gmail.com>
2025-04-19 18:27:03 +0200

    Update logger.py

--- logger.py
+++ logger.py
@@ -3,6 +3,14 @@
 import logging
 from datetime import datetime
 
+def log(message):
+    timestamp = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+    formatted_message = f"[{timestamp} UTC] {message}"
+    print(formatted_message)
+    with open("logs/bot_log.txt", "a") as f:
+        f.write(formatted_message + "\n")
+
+
 def setup_logger():
     log_file = f"logs/bot_{datetime.utcnow().strftime('%Y-%m-%d')}.log"
     logging.basicConfig(
