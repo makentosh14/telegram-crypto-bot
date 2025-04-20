2519a147a57b57402994ba262e85a538755a1b74
makentosh14 <rimgaudas.veliulis@gmail.com>
2025-04-20 11:28:20 +0200

    Update bollinger.py

--- bollinger.py
+++ bollinger.py
@@ -1,9 +1,21 @@
+import numpy as np
+
 def detect_bollinger_breakout(close_prices, window=20, num_std=2):
     if len(close_prices) < window:
         return False
 
-    sma = sum(close_prices[-window:]) / window
-    std_dev = (sum((x - sma) ** 2 for x in close_prices[-window:]) / window) ** 0.5
-    upper_band = sma + num_std * std_dev
+    recent_closes = np.array(close_prices[-window:])
+    sma = np.mean(recent_closes)
+    std_dev = np.std(recent_closes)
 
-    return close_prices[-1] > upper_band
+    upper_band = sma + num_std * std_dev
+    lower_band = sma - num_std * std_dev
+
+    last_price = close_prices[-1]
+
+    if last_price > upper_band:
+        return "breakout_up"
+    elif last_price < lower_band:
+        return "breakout_down"
+    else:
+        return None
