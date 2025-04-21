de15679730a5b076e4d567aca059da0085752385
makentosh14 <rimgaudas.veliulis@gmail.com>
2025-04-21 11:33:49 +0200

    Create watchlist_manager.py

---
+++ watchlist_manager.py
@@ -0,0 +1,15 @@
+# watchlist_manager.py
+
+watchlist = set()
+
+def add_to_watchlist(symbol):
+    watchlist.add(symbol.upper())
+
+def remove_from_watchlist(symbol):
+    watchlist.discard(symbol.upper())
+
+def get_watchlist():
+    return list(watchlist)
+
+def is_watched(symbol):
+    return symbol.upper() in watchlist
