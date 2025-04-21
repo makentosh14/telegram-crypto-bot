5025a946d5d09d84d24404006cadbcc36ce85872
makentosh14 <rimgaudas.veliulis@gmail.com>
2025-04-21 18:52:21 +0200

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 BYBIT_API_KEY = "tL7vmTEDT5B8mp4Yer"
 BYBIT_API_SECRET = "xH5S3U3dkLeQJ739cl9AZ0MMNQkerD53vAXN"
-BYBIT_API_URL = "https://api.bybit.com"
+BYBIT_API_URL = "https://api-testnet.bybit.com"
 
 
 TELEGRAM_BOT_TOKEN = "7803544014:AAGLJVwfTg4Ij5lzI8RIVRfrZkKG9uIZnh4"
