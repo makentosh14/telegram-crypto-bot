3d42e77c09d8b1d658fe61a678dcf120843017be
makentosh14 <rimgaudas.veliulis@gmail.com>
2025-08-17 21:21:27 +0200

    Update test_fix.py

--- test_fix.py
+++ test_fix.py
@@ -19,7 +19,6 @@ try:
         get_market_sentiment,
         calculate_ema_fixed,
         validate_short_signal,
-        validate_short_signal_fixed,
         monitor_btc_trend_accuracy,
         monitor_altseason_status,
         btc_analyzer,
